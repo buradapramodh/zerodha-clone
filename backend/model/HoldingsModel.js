52b3cc362ba714838d8a02bfaf5d663515dadf24
buradapramodh <143316696+buradapramodh@users.noreply.github.com>
2025-10-11 10:36:16 +0530

    Initial Zerodha Commit

---
+++ backend/model/HoldingsModel.js
@@ -0,0 +1,4 @@
+const { model }=require("mongoose");
+const { HoldingsSchema }=require("../schemas/HoldingsSchemas");
+const HoldingsModel=new model("holding",HoldingsSchema);
+module.exports={ HoldingsModel };
